c7d23162aecdbcd41461f4d31955af2eccf70a27
francescacastagnetti <122844853+francescacastagnetti@users.noreply.github.com>
2023-10-19 00:11:00 -0400

    DONE

--- JournalExercise/journal.js
+++ JournalExercise/journal.js
@@ -1,32 +1,3 @@
-// window.onload=function(){      
-//       document.getElementById("journal-form").addEventListener("submit", function (event) {
-            
-//             event.preventDefault(); // Prevent the default form submission behavior
-
-//             // Get input values
-//             var title = document.getElementById("entry-title").value;
-//             var text = document.getElementById("entry-text").value;
-//             var imageUrl = document.getElementById("entry-image-url").value;
-
-//             // Create a new entry element and display it
-//             var entry = document.createElement("div");
-//             entry.innerHTML = "<h2>" + title + "</h2><p>" + text + "</p>";
-
-//             // If an image URL is provided, display the image
-//             if (imageUrl) {
-//                 entry.innerHTML += "<img src='" + imageUrl + "' alt='" + title + "'>";
-//             }
-
-//             // Append the entry to the journal-entries section
-//             document.getElementById("journal-entries").appendChild(entry);
-
-//             // Clear the form fields for the next entry
-//             document.getElementById("entry-title").value = "";
-//             document.getElementById("entry-text").value = "";
-//             document.getElementById("entry-image-url").value = "";
-//       });
-// }
-
 
 var clearEntryForm = function(){
       document.getElementById("entryPhoto").value = "";
@@ -52,75 +23,11 @@ function addJournalEntry(){
             return;
       }
 
-      journalEntries = "\n <h2><span>${dateString}:</span> ${title}</h2> <div class='entireJournal'><img src='${newEntryPhoto}'>${newEntryContent}</div>";
+      journalEntries = `<div class="entry"><h2><span>${dateString}:</span> ${title}</h2> <div><img src="${newEntryPhoto}"><p class="pStyle">${newEntryContent}</p></div></div>` + journalEntries;
+      document.getElementById("entries").innerHTML = journalEntries;
 
 }
 
 
 
-
-// String.prototype.isEmpty = function(){
-//       return (this.length === 0 || !this.trim());
-// };
-
-// var guestMessages = {
-//       msgs: [],
-
-//       getMessages: function(msg){
-//           this.msgs.push("User: " + value + "<br />");
-
-//       },
-
-//       printMessages: function(){
-//           guestMessages.getMessages();
-//           var showEntry = document.getElementById("first-entry").innerHTML = this.msgs;
-
-//       }
-// };
-
-// var getEntry = function(){
-//       value = document.getElementById("content").value;
-//       return value;
-// };
-
-
-// var getURL = function() {
-//       var imageUrl = document.getElementById("URL").value;
-//       if (imageUrl) {
-//             entry.innerHTML += "<img src='" + imageUrl + "' alt='" + title + "'>";
-//       }
-//       return imageUrl;
-// }
-
-
-// var clearDiv = function(){
-//     document.getElementById("content").value = "";
-// }
-
-
-// var isEmpty = function(entry){
-//       entry = getEntry();
-//       // imgEntry = getURL();
-//       var output = document.getElementById("content").value;
-//       // var IMGoutput = document.getElementById("URL").value;
-//       if ( output == '' ){
-//                 alert("Enter a message");
-//       }
-//        else {
-//         guestMessages.printMessages();
-//       }
-// }
-
-
-// addAnotherJournal = function() {
-//       var ul = document.getElementById("list");
-//       var li = document.createElement("li");
-//       var children = ul.children.length + 1
-//       li.setAttribute("id", "element"+children)
-//       li.appendChild(document.createTextNode("Element "+children));
-//       ul.appendChild(li)
-//   }
-  
-
-// updateEntry();
 
---
+++ sepWords.js
@@ -0,0 +1,9 @@
+txt = "Monday is good";
+
+function stringToArray(text){
+    let textarray = text.split(" ");
+    return textarray;
+}
+
+alert(stringToArray(txt).toString());
+  
