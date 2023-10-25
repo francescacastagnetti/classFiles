6fa17351d1d312f3810881f5c9001d1e496da560
francescacastagnetti <122844853+francescacastagnetti@users.noreply.github.com>
2023-10-25 05:03:00 -0400

    hw4

--- JournalExercise/journal.js
+++ JournalExercise/journal.js
@@ -15,16 +15,16 @@ function addJournalEntry(){
       let dateString = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
       let title = document.getElementById("title").value;
       let newEntryPhoto = document.getElementById("entryPhoto").value;
-      let newEntryContent = document.getElementById("entryContent").value;
+      let newEntryContent = document.getElementById("entryContent").value; 
       let journalEntries = document.getElementById("entries").innerHTML;
 
       if(newEntryContent == ''){
-            alert("Error: cannot add an empty journal entry! \n\nPlease enter a message.");
+            alert("Error: cannot add an empty journal entry! \n\nPlease enter a message."); 
             return;
       }
 
-      journalEntries = `<div class="entry"><h2><span>${dateString}:</span> ${title}</h2> <div><img src="${newEntryPhoto}"><p class="pStyle">${newEntryContent}</p></div></div>` + journalEntries;
-      document.getElementById("entries").innerHTML = journalEntries;
+      journalEntries = `<div class="entry"><h3><span>${dateString}:</span> ${title}</h3> <div><img src="${newEntryPhoto}"><p class="pStyle">${newEntryContent}</p></div></div>` + journalEntries;
+      document.getElementById("entries").innerHTML = journalEntries; 
 
 }
 
