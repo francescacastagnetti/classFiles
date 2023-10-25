4ff4160b889f66778a17c790d136d8639c306c1d
francescacastagnetti <122844853+francescacastagnetti@users.noreply.github.com>
2023-10-25 11:27:33 -0400

    done

--- codingChallenges.js
+++ codingChallenges.js
@@ -1,210 +1,38 @@
-////////////////////////////////////
-// Coding Challenge #1
 
-/*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
-BMI = mass / height ** 2 = mass / (height * height). 
-(mass in kg and height in meter).
-
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both versions)
-3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
-
-TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
-
-GOOD LUCK 😀
-*/
-
-console.log("Coding Challenge #1 \n");
-
-let massMark = 78;
-let heightMark = 1.69;
-let massJohn = 92;
-let heightJohn = 1.95;
-
-let BMIMark = massMark / heightMark ** 2;
-let BMIJohn = massJohn / (heightJohn * heightJohn);
-let markHigherBMI = BMIMark > BMIJohn
-
-console.log("TEST DATA #1");
-console.log("Mark's BMI:", BMIMark, "\nJohn's BMI:", BMIJohn, "\nMark has a higher BMI than John:", markHigherBMI);
-
-massMark = 95;
-heightMark = 1.88;
-massJohn = 85;
-heightJohn = 1.76;
-
-BMIMark = massMark / heightMark ** 2;
-BMIJohn = massJohn / (heightJohn * heightJohn);
-markHigherBMI = BMIMark > BMIJohn;
-
-console.log("TEST DATA #2");
-console.log("Mark's BMI:", BMIMark, "\nJohn's BMI:", BMIJohn, "\nMark has a higher BMI than John:", markHigherBMI, "\n");
-
-
-////////////////////////////////////
-// Coding Challenge #2
-
-/*
-Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
-
-1. Print an output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
-2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
-
-HINT: Use an if/else statement 😉
-
-GOOD LUCK 😀
-*/
-
-console.log("Coding Challenge #2 \n");
-
-massMark = 78;
-heightMark = 1.69;
-massJohn = 92;
-heightJohn = 1.95;
-
-
-BMIMark = massMark / heightMark ** 2;
-BMIJohn = massJohn / (heightJohn * heightJohn);
-
-console.log("TEST DATA #1");
-if (BMIMark > BMIJohn) {
-	console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+function calcBMI(){
+  BMI1 = document.getElementById("M1").value / document.getElementById("H1").value ** 2;
+  BMI2 = document.getElementById("M2").value / document.getElementById("H2").value ** 2;
+  if (BMI1 > BMI2) {
+	  BMIs.innerText = (`Your BMI (${BMI1}) is higher than the other person's (${BMI2})!`);
+  } else if (BMI1 < BMI2) {
+	  BMIs.innerText = (`The other person's BMI (${BMI1}) is higher than yours (${BMI2})!`);
   } else {
-	console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
+    BMIs.innerText = (`Both BMIs are the same!`);
   }
+}  
   
-massMark = 95;
-heightMark = 1.88;
-massJohn = 85;
-heightJohn = 1.76;
-
-BMIMark = massMark / heightMark ** 2;
-BMIJohn = massJohn / (heightJohn * heightJohn);
-
-console.log("TEST DATA #2");
-if (BMIMark > BMIJohn) {
-	console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`, "\n")
-  } else {
-	console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`, "\n")
-  }
-
-////////////////////////////////////
-// Coding Challenge #3
-
-/*
-There are two gymnastics teams, Pandas and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
-
-1. Calculate the average score for each team, using the test data below
-2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
-
-3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
-4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
-
-TEST DATA: Pandas score 96, 108 and 89. Koalas score 88, 91 and 110
-TEST DATA BONUS 1: Pandas score 97, 112 and 101. Koalas score 109, 95 and 123
-TEST DATA BONUS 2: Pandas score 97, 112 and 101. Koalas score 109, 95 and 106
-
-GOOD LUCK 😀
-*/
-
-console.log("Coding Challenge #3 \n");
-
-console.log("TEST DATA");
-
-let scorePandas = (96 + 108 + 89) / 3;
-let scoreKoalas = (88 + 91 + 110) / 3;
-console.log("Pandas Average Score:", scorePandas, "\nKoalas Average Score:", scoreKoalas);
-
-if (scorePandas > scoreKoalas) {
-  console.log('Pandas win the trophy 🏆');
-} else if (scoreKoalas > scorePandas) {
-  console.log('Koalas win the trophy 🏆');
-} else if (scorePandas === scoreKoalas) {
-  console.log('Both win the trophy!');
-}
 
 
-let avgScore = function(scoreArray) {
-	let sum = 0;
-	for (let i in scoreArray) {
-// 	  console.log(sum, scoreArray[i]);
-	  sum = sum + scoreArray[i];
-	}
-	let result = sum / scoreArray.length;
-// 	console.log(result);
-	return result ;
-  }
-  
-  // BONUS 1
-console.log("TEST DATA BONUS #1");
 
-  let pandasScoreArray = [97, 112, 101];
-  scorePandas = avgScore(pandasScoreArray);
-  let koalasScoreArray = [109, 95, 123];
-  scoreKoalas = avgScore(koalasScoreArray);
-  console.log("Pandas Average Score:", scorePandas, "\nKoalas Average Score:", scoreKoalas);
+function compareTeams(){
+
+  scorePandas = (parseInt(document.getElementById("P1").value) + parseInt(document.getElementById("P2").value) + parseInt(document.getElementById("P3").value))/3;;
+  scoreKoalas = (parseInt(document.getElementById("K1").value) + parseInt(document.getElementById("K2").value) + parseInt(document.getElementById("K3").value))/3;;
   
   if (scorePandas > scoreKoalas && scorePandas >= 100) {
-	console.log('Pandas win the trophy 🏆');
+	  scores.innerText = ('Pandas win the trophy 🏆');
   } else if (scoreKoalas > scorePandas && scoreKoalas >= 100) {
-	console.log('Koalas win the trophy 🏆');
+    scores.innerText = ('Koalas win the trophy 🏆');
   } else if (scorePandas === scoreKoalas && scorePandas >= 100 && scoreKoalas >= 100) {
-	console.log('Both win the trophy!🏆🏆');
+    scores.innerText = ('Both win the trophy!🏆🏆');
   } else {
-	console.log('No one wins the trophy 😭');
+    scores.innerText = ('No one wins the trophy 😭');
   }
   
-  
-  console.log("TEST DATA BONUS #2");
-  pandasScoreArray = [97, 112, 101];
-  scorePandas = avgScore(pandasScoreArray);
-  koalasScoreArray = [109, 95, 106];
-  scoreKoalas = avgScore(koalasScoreArray);
-  console.log(scorePandas, scoreKoalas);
- 
-    if (scorePandas > scoreKoalas && scorePandas >= 100) {
-	console.log('Pandas win the trophy 🏆\n');
-  } else if (scoreKoalas > scorePandas && scoreKoalas >= 100) {
-	console.log('Koalas win the trophy 🏆\n');
-  } else if (scorePandas === scoreKoalas && scorePandas >= 100 && scoreKoalas >= 100) {
-	console.log('Both win the trophy! 🏆🏆 \n');
-  } else {
-	console.log('No one wins the trophy 😭 \n');
-  }
-  
+}
 
-////////////////////////////////////
-// Coding Challenge #4
-
-/*
-Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
-
-1. Your task is to calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
-2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
-
-TEST DATA: Test for the bill values 275, 40 and 430
-
-HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
-HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
-
-GOOD LUCK 😀
-*/
-
-console.log("Coding Challenge #4 \n");
-
-console.log("TEST DATA #1");
-let bill = 275;
-let tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}.`);
-
-console.log("TEST DATA #2");
-bill = 40;
-tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}.`);
-
-console.log("TEST DATA #3");
-bill = 430;
-tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}.`);
+function calcBill(){
+  bill = parseInt(document.getElementById("bill").value);
+  tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+  cost.innerText = (`The bill was $${bill}, the tip was $${tip}, and the total value $${bill + tip}.`);
+}
